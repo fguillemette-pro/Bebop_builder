9d69c0080018cf318a0257f776419d7a610cdad7
fguillemette-pro <87635070+fguillemette-pro@users.noreply.github.com>
2023-08-15 21:33:39 +0000

    feat: control root

--- main.sh
+++ main.sh
@@ -1,2 +1,23 @@
 #!/bin/bash
-echo -e 'Bebop Builder'
+
+# Variables
+VERSION='0.1'
+
+# Functions
+
+check_root() {
+    if [ "$EUID" -ne 0 ]; then
+        echo "Ce script doit être exécuté en tant que superutilisateur (root)." >&2
+        exit 1
+    fi
+}
+
+# Main
+
+clear
+echo -e '##################'
+echo -e 'Bebop Builder init'
+echo -e '##################'
+
+check_root
+echo "OK ROOT"
